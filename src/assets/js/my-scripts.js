bf473c5d80bf2f373409cb04ed0e884558fadf57
strdr4605 <str.dr4605@gmail.com>
2017-07-14 11:17:45 +0300

    add map, change figure styles, change theme color

--- src/assets/js/my-scripts.js
+++ src/assets/js/my-scripts.js
@@ -1,6 +1,21 @@
-$( document ).ready(function() {
+$(document ).ready(function() {
 
-  // Get started!
-  console.log('hi')
+  $(window).scroll(function () {
+    var wScroll = $(this).scrollTop()
 
-});
+    $('.poster').css({
+      'transform': 'translate(0px, ' + wScroll / 1.5 + 'px)'
+    })
+
+    $('.slogan').css({
+      'transform': 'translate(0px, ' + wScroll / 2.3 + 'px)'
+    })
+  })
+
+  $('#products figure').each(function (i) {
+    setTimeout(function () {
+      $('#products figure').eq(i).addClass('is-showing')
+    }, 150 * (i + 1))
+  })
+
+})
